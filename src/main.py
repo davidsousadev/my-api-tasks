5c56e564b528313ed0ed24983191e94381d34004
David Sousa <72691307+davidsousadev@users.noreply.github.com>
2024-12-11 11:07:09 -0300

    ajuste de rota

--- src/main.py
+++ src/main.py
@@ -26,7 +26,7 @@ app.include_router(registros_router,
                     prefix='/registros', 
                     tags=["Registros"])
                     
-app.include_router(user_router,
+app.include_router(users_router,
                    tags=["Users"])
 
 # Criar DB
